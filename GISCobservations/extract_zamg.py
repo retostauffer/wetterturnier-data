b219816bf863cbbb5bbb1e4dec820b3feb472f2d
Juri Hubrig <juri.hubrig@fu-berlin.de>
2023-05-12 22:27:27 +0200

    added get_zamg.sh and extract_zamg.py to replaces get/extract_fx scripts

---
+++ GISCobservations/extract_zamg.py
@@ -0,0 +1,75 @@
+#!/bin/python3
+
+import json, sys
+
+obs = {}
+stations = { 5904 : 11035, 5917 : 11040, 11803 : 11320, 11804 : 11120 }
+
+from datetime import datetime as dt, timedelta as td, timezone as tz
+fmt = "%Y-%m-%d"
+
+td1 = td(days = 1)
+
+if len(sys.argv) == 2:
+   DATE = sys.argv[1]
+   DATE = dt.strptime(DATE, fmt).replace( tzinfo = tz.utc )
+else:
+   from datetime import date
+   DATE = dt.utcnow().date() - td1
+
+path = "ZAMG/"
+
+none_counter = 0
+
+params = {"FF":"ff", "FFX":"fx", "RR":"rr", "SO":"sun" }
+
+with open( path + DATE.strftime(fmt) + ".json", "r" ) as f:
+   d = json.load(f)
+   for f in d["features"]:
+      p = f["properties"]
+      none_counter = 0
+      for param in list(params.keys()):
+         try:
+            obs[stations[int(p["station"])]] = p["parameters"][param]["data"]
+         except:
+            obs[stations[int(p["station"])]] = None
+            none_counter += 1
+
+      if none_counter == len(params.keys()):
+         print("MISSING DATA!")
+
+print(obs)
+sys.exit()
+
+sys.path.append('PyModules')
+from readconfig import readconfig
+config = readconfig("config.conf")
+
+from database import database
+db = database(config)
+cur = db.cursor()
+
+#add column
+sql = "ALTER TABLE `live` ADD IF NOT EXISTS `fx24` SMALLINT(5) NULL DEFAULT NULL"
+cur.execute(sql)
+
+sql = []
+
+today = DATE + td1
+datum = today.strftime("%Y%m%d")
+datumsec = dt.combine( today, dt.min.time() )
+datumsec = int( datumsec.replace( tzinfo = tz.utc ).timestamp() )
+print(datumsec)
+
+#insert obs
+for i,f in enumerate(fx):
+   try:
+      FX = int(fx[f]*10)
+   except:
+      FX = 'null'
+   sql.append( f"INSERT INTO live (statnr,datum,datumsec,stdmin,msgtyp,fx24) VALUES ({f},{datum},{datumsec},0,'bufr',{FX}) ON DUPLICATE KEY UPDATE ucount=ucount+1, stdmin=VALUES(stdmin), fx24=VALUES(fx24);" )
+
+print(sql)
+
+for s in sql:
+   cur.execute( s )
---
+++ GISCobservations/get_zamg.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#receive the fx values for Austrian WMO stations from ZAMG OpenData
+
+base="https://dataset.api.hub.zamg.ac.at/v1/station/historical/"
+#resource_id="klima-v1-1d"
+resource_id="klima-v1-10min"
+parameters="FF,FFX,RR,SO"
+station_ids="5904,5917,11803,11804"
+start=${1:-$(date +%Y-%m-%d --date="1 day ago")}
+if [ -z $2 ]; then
+   end=$(date +%Y-%m-%d)
+else
+   end=$2
+fi
+
+mkdir -p ZAMG
+
+wget "${base}${resource_id}?parameters=${parameters}&start=${start}&end=${end}&station_ids=${station_ids}" -O "ZAMG/${start}.test.json" #&& python3 extract_zamg.py ${start}
